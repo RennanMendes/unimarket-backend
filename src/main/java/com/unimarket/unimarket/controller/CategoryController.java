af2ec0d1ee2053f986bef08158aae6cbe3be20e6
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2024-02-28 21:00:42 -0300

    feat: created category controller

---
+++ src/main/java/com/unimarket/unimarket/controller/CategoryController.java
@@ -0,0 +1,49 @@
+package com.unimarket.unimarket.controller;
+
+import com.unimarket.unimarket.application.CreateCategoryUseCaseImpl;
+import com.unimarket.unimarket.application.DeleteCategoryUseCaseImpl;
+import com.unimarket.unimarket.core.cases.category.FindAllCategoriesUseCase;
+import com.unimarket.unimarket.core.entities.category.Category;
+import com.unimarket.unimarket.infra.entity.CategoryEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    private final CreateCategoryUseCaseImpl createCategory;
+    private final DeleteCategoryUseCaseImpl deleteCategory;
+    private final FindAllCategoriesUseCase findAllCategories;
+
+    @Autowired
+    public CategoryController(CreateCategoryUseCaseImpl createCategory, DeleteCategoryUseCaseImpl deleteCategory, FindAllCategoriesUseCase findAllCategories) {
+        this.createCategory = createCategory;
+        this.deleteCategory = deleteCategory;
+        this.findAllCategories = findAllCategories;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<CategoryEntity>> findAll(){
+        List<CategoryEntity> categories = this.findAllCategories.findAll();
+        return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping()
+    public ResponseEntity<CategoryEntity> create(@RequestBody Category category, UriComponentsBuilder uriBuilder) {
+        CategoryEntity response = this.createCategory.create(category);
+        URI uri = uriBuilder.path("/category/create/{id}").buildAndExpand(response.getId()).toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @DeleteMapping("/{categoryName}")
+    public ResponseEntity delete(@PathVariable String categoryName){
+        this.deleteCategory.delete(categoryName);
+        return ResponseEntity.noContent().build();
+    }
+}
